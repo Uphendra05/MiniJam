49e16dbfaed23f4db11511780e729d0d03d00828
mohamedibrahim155 <ibrahim155.developer@gmail.com>
2021-04-03 01:25:12 +0530

    EndPoint Script added

---
+++ Assets/Scripts/EndPoint.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EndPoint : MonoBehaviour
+{
+    public float waitTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            Debug.Log("Player in");
+            StartCoroutine(waiting(waitTime));
+            this.GetComponent<Collider>().enabled = false;
+            other.gameObject.GetComponent<Ball>().enabled = false;
+         
+
+        }
+    }
+  
+    public IEnumerator waiting(float _waitTime)
+    {
+        yield return new WaitForSeconds(_waitTime);
+       
+    }
+
+   
+}
